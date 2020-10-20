cdc9839bf1fee321dc7e10988bfd49df2adf22e3
Angel Afonso <angelafonso60@gmail.com>
2020-10-20 18:32:34 -0400

    initialized project and added lexer advance and some docs

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+#![allow(dead_code)] // temporal
+mod lexer;
+mod token;
+
+fn main() {
+    println!("hello world");
+}
---
+++ src/token.rs
@@ -0,0 +1,80 @@
+/// the tokens are a representation of the identifiers found in the input code
+#[derive(Debug, Clone)]
+pub enum Token {
+    Illegal,
+    EndOfFile, // EOF
+
+    /// Represents an identifier, for example: `foo`, `bar`
+    Ident(String),
+    /// Represents an integer literal
+    Int(String),
+
+    // Delimeters
+    /// Represents a `,` character
+    Comma,
+    /// Represents a `;` character
+    Semicolon,
+    /// Represents a `(` character
+    LParen,
+    /// Represents a `)` character
+    RParen,
+    /// Represents a `{` character
+    LBrace,
+    /// Represents a `}` character
+    RBrace,
+
+    // Keywords
+    /// Represents a `function` keyword
+    Function,
+    /// Represents a `let` keyword
+    Let,
+    /// Represents a `true` keyword
+    True,
+    /// Represents a `false` keyword
+    False,
+    /// Represents a `if` keyword
+    If,
+    /// Represents a `else` keyword
+    Else,
+    /// Represents a `return` keyword
+    Return,
+    /// Represents a `for` keyword
+    For,
+
+    // Operators
+    /// Represents a `=` operator
+    Assign,
+    /// Represents a `+` operator
+    Plus,
+    /// Represents a `-` operator
+    Minus,
+    /// Represents a `!` operator
+    Bang,
+    /// Represents a `*` operator
+    Asterisk,
+    /// Represents a `/` operator
+    Slash,
+    /// Represents a `<` operator
+    LowerThan,
+    /// Represents a `>` operator
+    GreaterThan,
+    /// Represents a `==` operator
+    Equal,
+    /// Represents a `!=` operator
+    NotEqual,
+}
+
+/// look if `ident` is a keyword
+pub fn look_ident(ident: &str) -> Token {
+    match ident {
+        "fn" => Token::Function,
+        "let" => Token::Let,
+        "true" => Token::True,
+        "false" => Token::False,
+        "return" => Token::Return,
+        "if" => Token::If,
+        "else" => Token::Else,
+        "for" => Token::For,
+        _ => Token::Ident(ident.to_string()),
+    }
+}
